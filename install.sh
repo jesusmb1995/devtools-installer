4ca6e598828cc03354974e4faaedc5557d1017d1
Test User <test@example.com>
2026-08-16 16:10:18 +0200

    copals generate @ 2026-08-16T14:10:18Z

--- install.sh
+++ install.sh
@@ -1983,8 +1983,8 @@ cmd_update__431_v0() {
             local parts_879=("${ret_split4_v0[@]}")
             local __length_57=("${parts_879[@]}")
             if [ "$(( ${#__length_57[@]} >= 2 ))" != 0 ]; then
-                old_keys_871+=("${parts_879[0]?"Index out of bounds (at /tmp/jbtd-install-build-9g7jRu/install.ab:371:36)"}")
-                old_vals_872+=("${parts_879[1]?"Index out of bounds (at /tmp/jbtd-install-build-9g7jRu/install.ab:372:36)"}")
+                old_keys_871+=("${parts_879[0]?"Index out of bounds (at /tmp/jbtd-install-build-uLhh7z/install.ab:371:36)"}")
+                old_vals_872+=("${parts_879[1]?"Index out of bounds (at /tmp/jbtd-install-build-uLhh7z/install.ab:372:36)"}")
             fi
         done
     fi
@@ -2001,16 +2001,16 @@ cmd_update__431_v0() {
         if [ "$(( ${#__length_63[@]} < 2 ))" != 0 ]; then
             continue
         fi
-        local key_884="${parts_883[0]?"Index out of bounds (at /tmp/jbtd-install-build-9g7jRu/install.ab:383:27)"}"
-        local val_885="${parts_883[1]?"Index out of bounds (at /tmp/jbtd-install-build-9g7jRu/install.ab:384:27)"}"
+        local key_884="${parts_883[0]?"Index out of bounds (at /tmp/jbtd-install-build-uLhh7z/install.ab:383:27)"}"
+        local val_885="${parts_883[1]?"Index out of bounds (at /tmp/jbtd-install-build-uLhh7z/install.ab:384:27)"}"
         local matched_886=0
         local __range_start_887=0
         local __length_64=("${old_keys_871[@]}")
         local __range_end_887="${#__length_64[@]}"
         local __dir_887=$(( ${__range_start_887} <= ${__range_end_887} ? 1 : -1 ))
         for (( i_887=${__range_start_887}; i_887 * ${__dir_887} < ${__range_end_887} * ${__dir_887}; i_887+=${__dir_887} )); do
-            if [ "$([ "_${old_keys_871[${i_887}]?"Index out of bounds (at /tmp/jbtd-install-build-9g7jRu/install.ab:387:25)"}" != "_${key_884}" ]; echo $?)" != 0 ]; then
-                if [ "$([ "_${old_vals_872[${i_887}]?"Index out of bounds (at /tmp/jbtd-install-build-9g7jRu/install.ab:388:29)"}" == "_${val_885}" ]; echo $?)" != 0 ]; then
+            if [ "$([ "_${old_keys_871[${i_887}]?"Index out of bounds (at /tmp/jbtd-install-build-uLhh7z/install.ab:387:25)"}" != "_${key_884}" ]; echo $?)" != 0 ]; then
+                if [ "$([ "_${old_vals_872[${i_887}]?"Index out of bounds (at /tmp/jbtd-install-build-uLhh7z/install.ab:388:29)"}" == "_${val_885}" ]; echo $?)" != 0 ]; then
                     local array_65=("${key_884}")
                     changed_880+=("${array_65[@]}")
                 fi
@@ -2031,7 +2031,7 @@ done
             split__4_v0 "	" "${row_890}"
             local parts_891=("${ret_split4_v0[@]}")
             local __length_71=("${parts_891[@]}")
-            if [ "$(( $(( ${#__length_71[@]} >= 2 )) && $([ "_${parts_891[0]?"Index out of bounds (at /tmp/jbtd-install-build-9g7jRu/install.ab:405:42)"}" != "_${ok_888}" ]; echo $?) ))" != 0 ]; then
+            if [ "$(( $(( ${#__length_71[@]} >= 2 )) && $([ "_${parts_891[0]?"Index out of bounds (at /tmp/jbtd-install-build-uLhh7z/install.ab:405:42)"}" != "_${ok_888}" ]; echo $?) ))" != 0 ]; then
                 found_889=1
                 break
             fi
@@ -2122,7 +2122,7 @@ if [ "$(( ${#__length_89[@]} == 0 ))" != 0 ]; then
 fi
 __length_90=("${step_args_120[@]}")
 if [ "$(( ${#__length_90[@]} >= 1 ))" != 0 ]; then
-    if [ "$([ "_${step_args_120[0]?"Index out of bounds (at /tmp/jbtd-install-build-9g7jRu/install.ab:524:22)"}" != "_update" ]; echo $?)" != 0 ]; then
+    if [ "$([ "_${step_args_120[0]?"Index out of bounds (at /tmp/jbtd-install-build-uLhh7z/install.ab:524:22)"}" != "_update" ]; echo $?)" != 0 ]; then
         __length_91=("${step_args_120[@]}")
         if [ "$(( ${#__length_91[@]} == 1 ))" != 0 ]; then
             cmd_update__431_v0 
